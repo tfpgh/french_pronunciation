469bd6d43e6261997a55d960fe264c5d212976d1
Toby Penner <tobypenner01@gmail.com>
2026-01-31 16:45:20 -0500

    Add mfa in docker in slurm

---
+++ slurm/run_mfa.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=run_mfa
+#SBATCH --output=logs/%j.out
+#SBATCH --partition=standard
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=128gb
+#SBATCH --time=2-00:00:00
+
+podman pull mmcauliffe/montreal-forced-aligner:latest
+
+mkdir -p /storage/tpenner/mfa_data
+
+podman run --rm -it \
+    --userns=keep-id \
+    -v /storage/penner:/storage/tpenner \
+    -e MFA_ROOT_DIR=/storage/tpenner/mfa_data \
+    docker.io/mmcauliffe/montreal-forced-aligner:latest \
+    mfa model download dictionary french_mfa
+
+podman run --rm -it \
+    --userns=keep-id \
+    -v /storage/penner:/storage/tpenner \
+    -e MFA_ROOT_DIR=/storage/tpenner/mfa_data \
+    docker.io/mmcauliffe/montreal-forced-aligner:latest \
+    mfa model download acoustic french_mfa
